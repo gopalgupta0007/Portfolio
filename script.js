d4c1fbbc31c59ede8035708fdd31fb0ab52e3163
gopalgupta0007 <guptagopal18082003@gmail.com>
2023-04-21 21:24:27 +0530

    Updating navbar

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
 
 class Navbar {
-    static navAjax(e){
+    static navAjax(e) {
         console.log("navAjax started");
-        let url = ["Certificates", "services", "contectMe", "about"] 
+        let url = ["Certificates", "services", "contectMe", "about"]
         const xhttp = new XMLHttpRequest();
-        xhttp.onprogress = function() {
+        xhttp.onprogress = function () {
             document.getElementById("container").innerHTML = "Waiting...";
         }
-        xhttp.onload = function () { 
+        xhttp.onload = function () {
             document.getElementById("sup-container").innerHTML = this.responseText;
         }
-        xhttp.open("GET", `./NavElements/${url[e-2]}/${url[e-2]}.html`);
-        console.log(`${url[e-2]}.html`);
+        xhttp.open("GET", `./NavElements/${url[e - 2]}/${url[e - 2]}.html`);
+        console.log(`${url[e - 2]}.html`);
         xhttp.send();
     }
-    
+
     static navSelection(e) {
         /*this method is used to manage the style of nav-bar*/
         console.log("navSelection")
-        document.querySelectorAll("a")[1].removeAttribute("style");
-        // if (document.activeElement.tagName == 'A') {
-        //     document.getElementsByClassName("active").removeAttribute("class");
-        // }
-        let a = document.getElementsByTagName("a")[e];
-        a.setAttribute("class", "active");
+        for (let index = 1; index <= 5; index++) {
+            document.querySelectorAll("a")[index].removeAttribute("style");
+        }
+        document.querySelectorAll("a")[e].style.cssText = " color: #fff;\
+                                                            background: blue;\
+                                                            border-radius: 10px;\
+                                                            cursor: pointer;\
+                                                            text-shadow: 0 -2px 5px yellow;";
         Navbar.navAjax(e);
     }
 }
